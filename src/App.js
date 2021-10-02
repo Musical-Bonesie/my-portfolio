814bac11272cb137dcc2261e8e412f7d1006dd38
Musical-Bonesie <83307690+Musical-Bonesie@users.noreply.github.com>
2021-10-01 18:54:51 -0600

    added BrowserRouter to App.js and Routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route } from "react-router-dom";
+import EnglishHome from "./pages/EnglishHome/index";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Route exact path="/" component={EnglishHome} />
+      <Route exact path="/home-eng" component={EnglishHome} />
+      <Route exact path="/home-fr" component={EnglishHome} />
+    </BrowserRouter>
   );
 }
 
